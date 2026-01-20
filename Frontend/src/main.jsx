a9cb884185c695c9cc2dbed13c632e352f59f07a
Nalin <nalinjajoria@gmail.com>
2026-01-21 01:41:37 +0530

    API update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,13 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+  resources: {},
+  lng: "en",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
